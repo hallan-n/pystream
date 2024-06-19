e6477a089771c35c9574b2ef3ab765711890db20
hallan-n <hallanneves01@gmail.com>
2024-06-18 23:24:49 -0300

    test: add plan model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from app.domain.models.login import Login, LoginSignIn, LoginSignUp
 from app.domain.models.profile import Profile, ProfileCreate
+from app.domain.models.plan import Plan, PlanCreate
 
 
 def test_login():
@@ -15,3 +16,7 @@ def test_login():
 def test_profile():
     assert Profile(id=1, name="teste", icon="teste", login_id=1)
     assert ProfileCreate(icon="teste", name="teste")
+
+def test_plan():
+    assert Plan(id=1, max_profiles=3, name='teste')
+    assert PlanCreate(max_profiles=3, name='teste')
